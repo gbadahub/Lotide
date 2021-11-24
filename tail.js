a729fbb20ccfd7c53bdd3062340c6c4cba98853a
Georgina Bada <georginabada@gmail.com>
2021-11-23 21:48:47 -0500

    Tail

---
+++ tail.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected){
+    console. log ( `Assertion Passed: ${actual} === ${expected} `)
+  } else {
+    console.log (`❌Assertion Failed: ${actual} !== ${expected} ` )
+  }
+};
+
+// TEST CODE
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
